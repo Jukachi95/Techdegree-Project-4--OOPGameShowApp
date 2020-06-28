711bf1aecd84393e5df097312eec7ca2ee909950
Joel Okoro <jukachi95@gmail.com>
2020-06-28 19:34:41 +0100

    Initial solution for addPhraseToDisplay with forEach

--- js/Phrase.js
+++ js/Phrase.js
@@ -16,7 +16,31 @@
 
         phraseDisplay.forEach((phrase)=>{
 
+                // get the phrase
+                // split the phrase into separate divs
+
+                let splitPhrase = phrase.split
+
+                let displayPhrase = `
+                <div id="phrase" class="section">
+                      <ul>
+                             <li class="hide letter h">${splitPhrase[0]}</li>
+                             <li class="hide letter o">splitPhrase</li>
+                            <li class="hide letter w">w</li>
+                             <li class="space"> </li>
+                              <li class="hide letter a">a</li>
+                              <li class="hide letter r">r</li>
+                             <li class="hide letter e">e</li>
+                             <li class="space"> </li>
+                             <li class="hide letter y">y</li>
+                             <li class="hide letter o">o</li>
+                              <li class="hide letter u">u</li>
+                     </ul>
+            </div>
                 
+                `
+
+                return displayPhrase
 
         })
 
