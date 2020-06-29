d5da3b43c12c703c7f2983c2ed9f2cad206a74fc
Joel Okoro <jukachi95@gmail.com>
2020-06-29 20:23:46 +0100

    Setting attributes of splitPhrase items

--- js/Phrase.js
+++ js/Phrase.js
@@ -9,38 +9,51 @@
 
 
 
-     // Adds placeholders to the display
-     addPhraseToDisplay(){
-
+        // Adds placeholders to the display
+        addPhraseToDisplay(){
         
 
-        // Select the phrase
-        let phraseDisplay = this.phrase;
+             // Select the phrase
+             let phraseDisplay = this.phrase;
 
-        // Create list items that will be added to phrase 
-        // ** let listItem = document.createElement('li'); **
+             // Create list items that will be added to phrase 
+             // ** let listItem = document.createElement('li'); **
 
-        for(let i = 0; i < phraseDisplay.length; i++){
+       
 
-            // Select the ul to where the phrases will be inserted
-           let ul = document.querySelector('ul')
+             // Select the ul to where the phrases will be inserted
+             let ul = document.querySelector('ul')
 
-            // Split the phrase to get individual letters
-            let splitPhrase = phraseDisplay.split("")
+             // Create list items that will be added to phrase 
+            //  let listItem = document.createElement('li')
+
+             // Split the phrase to get individual letters
+             let splitPhrase = phraseDisplay.split("")
+
 
             // If there is a space in the phrase, add the space class, if not add hidden class 
             
-            if()
+                    for(let i = 0; i < splitPhrase.length; i++){
+                        if(splitPhrase[i] == ""){
+                            // If it is a space, add the "space class"
+                            splitPhrase[i].setAttribute('class', 'space')
+
+                        } else{
+                            // If it is a letter, add the "letter" class
+                            splitPhrase[i].setAttribute('class', 'letter')
+                        }
+                    }
 
 
-             let renderedList = `
-             <div id="phrase" class="section">
-             <li class = "letters">${phraseDisplay[i]}</li>`
-             return renderedList
+                        let rendered = `
+                        <div id="phrase" class="section">
+                        <li class = "letters">${splitPhrase[i]}</li>`
+
+                        ul.appendChild(rendered)
 
             
 
-        }
+    
 
         
 
