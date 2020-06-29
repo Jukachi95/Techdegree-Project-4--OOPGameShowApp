961c1be8c9da1708a0e83ac2074241002c2a69d0
Joel Okoro <jukachi95@gmail.com>
2020-06-29 21:25:14 +0100

    Formatting code for Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -13,44 +13,43 @@
         addPhraseToDisplay(){
         
 
-             // Select the phrase
-             let phraseDisplay = this.phrase;
+                    // Select the phrase
+                    let phraseDisplay = this.phrase;
 
-             // Create list items that will be added to phrase 
-             // ** let listItem = document.createElement('li'); **
-
-       
-
-             // Select the ul to where the phrases will be inserted
-             let ul = document.querySelector('ul')
-
-             // Create list items that will be added to phrase 
-            //  let listItem = document.createElement('li')
-
-             // Split the phrase to get individual letters
-             let splitPhrase = phraseDisplay.split("")
+                    // Create list items that will be added to phrase 
+                    // ** let listItem = document.createElement('li'); **
 
 
-            // If there is a space in the phrase, add the space class, if not add hidden class 
-            
-                    for(let i = 0; i < splitPhrase.length; i++){
-                        if(splitPhrase[i] == ""){
-                            // If it is a space, add the "space class"
-                            splitPhrase[i].setAttribute('class', 'space')
+                    // Select the ul to where the phrases will be inserted
+                    let ul = document.querySelector('ul')
 
-                        }else{
-                            // If it is a letter, add the "letter" class
-                            splitPhrase[i].setAttribute('class', 'letter')
-                        }
-                    }
+                    // Create list items that will be added to phrase 
+                    //  let listItem = document.createElement('li')
+
+                    // Split the phrase to get individual letters
+                    let splitPhrase = phraseDisplay.split("")
 
 
-                        // let rendered = `
-                        
-                        // <li class = "">${splitPhrase[i]}</li>`
 
-                        // ul.appendChild(rendered)
+                // If there is a space in the phrase, add the space class, if not add hidden class 
+                
+                                for(let i = 0; i < splitPhrase.length; i++){
+                                    if(splitPhrase[i] == ""){
+                                        // If it is a space, add the "space class"
+                                        splitPhrase[i].setAttribute('class', 'space')
 
+                                    }else{
+                                        // If it is a letter, add the "letter" class
+                                        splitPhrase[i].setAttribute('class', 'letter')
+                                    }
+                                }
+
+
+                                // let rendered = `
+                                
+                                // <li class = "">${splitPhrase[i]}</li>`
+
+                                // ul.appendChild(rendered)
 
      }
 
